5d927ae46149b5d10047d3a255e5f27b92e2df5e
Satyam <satyamsingh26002@gmail.com>
2026-02-14 17:19:15 +0530

    initial commit

--- EvenPrime.java
+++ EvenPrime.java
@@ -1,12 +1,12 @@
 public class EvenPrime {
     public static void main(String[] args) {
+        System.out.println("The Even Prime number are:");
         for(int i = 2; i <= 20; i++) {
             if(i % 2 == 0 && isPrime(i)) {
                 System.out.println(i);
             }
         }
     }
-
     static boolean isPrime(int n) {
         if(n < 2) return false;
         for(int i = 2; i <= Math.sqrt(n); i++) {
